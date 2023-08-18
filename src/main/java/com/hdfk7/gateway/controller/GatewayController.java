3784a763eef60ae06a1aec0a6e3129f6caba6b34
qiang.liang <173228514@qq.com>
2023-08-18 08:55:08 +0800

    feat:增加根路径重定向到api文档地址

---
+++ src/main/java/com/hdfk7/gateway/controller/GatewayController.java
@@ -0,0 +1,22 @@
+package com.hdfk7.gateway.controller;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.reactive.result.view.Rendering;
+
+@Tag(name = "网关接口")
+@Slf4j
+@Controller
+@RequestMapping
+public class GatewayController {
+
+    @Operation(summary = "根路径重定向到api文档")
+    @GetMapping
+    public Rendering toDoc() {
+        return Rendering.redirectTo("doc.html").build();
+    }
+}
